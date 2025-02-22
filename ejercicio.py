ec8b88cfe10ee9e866229bb56f00bcefd6be344d
JohannaB850 <johannabp850@gmail.com>
2025-02-21 20:10:30 -0500

    Create ejercicio.py

---
+++ ejercicio.py
@@ -0,0 +1,9 @@
+df = pd.read_csv("")
+st.title("Titulo x")
+df.drop(columns=["Unnamed: 0"], inplace=True)
+# Ver información general del dataset
+df.info()
+st.write("### Distribución de la Variable Balance") fig, ax = plt.subplots(figsize=(8, 5)) sns.histplot(df["Balance"], bins=30, kde=True, color='blue', ax=ax) ax.set_title("Distribución de la Variable Balance") ax.set_xlabel("Balance") ax.set_ylabel("Frecuencia") 
+st.pyplot(fig)
+st.write("### Matriz de Correlación") fig, ax = plt.subplots(figsize=(10, 6)) sns.heatmap(df.corr(numeric_only=True), annot=True, cmap="coolwarm", fmt=".2f", linewidths=0.5, ax=ax) 
+st.pyplot(fig)
